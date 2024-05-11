7cb4e0b32b17552afc49660b9530f9afd65f314b
Md Moaj <mdmoaj@hotmail.com>
2024-05-11 16:14:14 +0530

    first api test

---
+++ app/Http/Controllers/TerraboxController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TerraboxController extends Controller
+{
+    public function download(Request $request)
+    {
+
+        dd($request);
+
+
+    }
+}
